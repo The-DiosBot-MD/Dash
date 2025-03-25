43ad811d4e11c803e2210b0d71149b9849d71e15
camwhit-e <cmrxn2005@gmail.com>
2025-03-25 23:14:40 +0000

    Do not allow access to admin UI if user is not rootAdmin; closes #394

--- app/Http/Controllers/Admin/BaseController.php
+++ app/Http/Controllers/Admin/BaseController.php
@@ -3,12 +3,20 @@
 namespace Everest\Http\Controllers\Admin;
 
 use Illuminate\View\View;
+use Illuminate\Http\Request;
 use Everest\Http\Controllers\Controller;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class BaseController extends Controller
 {
-    public function index(): View
+    public function index(Request $request): View
     {
+        $user = $request->user();
+
+        if (!$user || !$user->root_admin) {
+            throw new AccessDeniedHttpException('You do not have permission to access this resource.');
+        }
+
         return view('templates/base.core');
     }
 }
