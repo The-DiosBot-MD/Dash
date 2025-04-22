f69b8e28390e7e8e06251ae59e5e8539c59ceb26
camwhit-e <cmrxn2005@gmail.com>
2025-04-22 23:02:28 +0100

    Fix SSH key list appearance

--- resources/scripts/components/dashboard/ssh/AccountSSHContainer.tsx
+++ resources/scripts/components/dashboard/ssh/AccountSSHContainer.tsx
@@ -49,13 +49,15 @@ export default () => {
                         data.map((key, index) => (
                             <GreyRowBox
                                 key={key.fingerprint}
-                                css={[tw`bg-neutral-600 flex space-x-4 items-center`, index > 0 && tw`mt-2`]}
+                                css={[tw`bg-black/50 flex space-x-4 items-center`, index > 0 && tw`mt-2`]}
                             >
                                 <FontAwesomeIcon icon={faKey} css={tw`text-neutral-300`} />
                                 <div css={tw`flex-1`}>
-                                    <p css={tw`text-sm break-words font-medium`}>{key.name}</p>
-                                    <p css={tw`text-xs mt-1 font-mono truncate`}>SHA256:{key.fingerprint}</p>
-                                    <p css={tw`text-xs mt-1 text-neutral-300 uppercase`}>
+                                    <p css={tw`text-lg font-bold break-words`}>{key.name}</p>
+                                    <p css={tw`text-xs mt-1 font-mono truncate text-gray-300`}>
+                                        SHA256:{key.fingerprint}
+                                    </p>
+                                    <p css={tw`text-xs mt-1 text-gray-400 uppercase`}>
                                         Added on:&nbsp;
                                         {key.created_at.toLocaleString()}
                                     </p>
