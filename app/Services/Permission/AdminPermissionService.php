b7b40d15bc0dd77d0abb24949abcfc017bef53a8
cmrxnn <cmrxn2005@gmail.com>
2025-05-03 20:19:42 +0100

    Create admin permission finder service

---
+++ app/Services/Permission/AdminPermissionService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Everest\Services\Billing;
+
+use Everest\Models\User;
+use Everest\Models\Billing\Order;
+use Everest\Models\Billing\Product;
+
+class AdminPermissionService
+{
+    /**
+     * Process the creation of an order.
+     */
+    public function handle(User $user): array
+    {
+        $permissions = [];
+
+        if ($user->admin_role_id) {
+            $role = AdminRole::findOrFail($user->admin_role_id);
+            $permissions[] = $role->permissions;
+        } else {
+            $permissions[] = ['*'];
+        };
+
+        return $permissions;
+    }
+}
